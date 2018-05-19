034e1c9bb16c0d177122a79ed7c2e27b0571d9c2
awokelee <awokelee@gmail.com>
2018-05-19 16:17:20 +0800

    header

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
+
---
+++ travel/src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont icon-fanhui back-icon"></span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont icon-sousuo"></span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      <span class="iconfont icon-solidArrow2-down arrow-icon"></span>
+      城市
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    background: $bgColor
+    line-height: .86rem
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      margin-top: .12rem
+      height: .64rem
+      line-height: .64rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.04rem
+      padding: 0 .1rem
+      float: right
+      text-align: center
+      color: #fff
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+</style>
