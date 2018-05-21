bc984f2b6a968ae8557255823c767b7fedd5a1ed
awokelee <awokelee@gmail.com>
2018-05-21 23:54:44 +0800

    animation

--- travel/src/pages/detail/components/Banner.vue
+++ travel/src/pages/detail/components/Banner.vue
@@ -7,12 +7,15 @@
         <div class="banner-number"><span class="iconfont icon-sousuo banner-icon"></span>{{gallaryImgs.length}}</div>
       </div>
     </div>
-    <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    <fade-animation>
+      <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -21,7 +24,8 @@ export default {
     gallaryImgs: Array
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   data () {
     return {
