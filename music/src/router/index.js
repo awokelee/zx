0347362f957f1d6da726f2b5a02afe25c54755aa
awokelee <604326541@qq.com>
2018-05-26 10:22:05 +0800

    router init

--- music/src/router/index.js
+++ music/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
+import Search from 'components/search/search'
+import Recommend from 'components/recommend/recommend'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    { path: '/', redirect: '/recommend' },
+    { path: '/recommend', ame: 'Recommend', component: Recommend },
+    { path: '/rank', ame: 'Rank', component: Rank },
+    { path: '/singer', ame: 'Singer', component: Singer },
+    { path: '/search', ame: 'Search', component: Search }
   ]
 })
