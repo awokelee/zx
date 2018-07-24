cbd8bf7d82c2a4810fa2170ec8e929b1ffc0d8e1
awokelee <awokelee@gmail.com>
2018-07-24 18:49:43 +0800

    feat: booklet

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -14,6 +14,16 @@ module.exports = {
       { text: 'Vue', link: '/vue/' },
       { text: 'Notes', link: '/notes/' },
       { text: 'Standard', link: '/standard/' },
+      { 
+        text: 'Booklet', 
+        items: [
+          { text: '用 npm script 打造超溜的前端工作流', link: '/standard0/' },
+          { text: '大厂 UI 开发实战手册', link: '/standard1/' },
+          { text: 'Git 原理详解及实用指南', link: '/standard2/' },
+          { text: '剖析 Vue.js 内部运行机制', link: '/standard3/' },
+          { text: 'Web 前端面试指南与高频考题解析', link: '/standard4/' },
+        ] 
+      },
       // { text: 'Guide', link: '/guide/' },
       {
         text: 'Projects',
@@ -114,6 +124,30 @@ module.exports = {
         '08_图片命名参考',
         '09_图片的命名规范及脚本文件和动态文件命名规范',
         '10_代码review检查点',
+      ],
+      // 小册
+      '/booklet/': [
+        '',
+        {
+          title:'用 npm script 打造超溜的前端工作流', 
+          children: [
+            '00_为什么选择npm_script',
+            /* '01_入门篇_创建并运行npm-script命令',
+            '02_入门篇_运行多个npm-script的各种姿势',
+            '03_入门篇_给npm-script传递参数和添加注释',
+            '04_进阶篇_使用npm-script的钩子',
+            '05进阶篇_在npm-script中使用环境变量',
+            '06_进阶篇_实现npm-script命令自动补全',
+            '07_高阶篇_实现npm-script跨平台兼容',
+            '08_高阶篇_把庞大的npm-script拆到单独文件中',
+            '09_高阶篇_用nodejs脚本替代复杂的npm-script',
+            '10_实战篇_监听文件变化并自动运行npm-script',
+            '11_实战篇_结合live-reload实现自动刷新',
+            '12_实战篇_在git-hooks中运行npm-script',
+            '13_实战篇_用npm-script实现构建流水线',
+            '14_实战篇_用npm-script实现服务自动化运维', */
+        ]},
+        // '01_commit-message规范',
       ],
       '/': [
         '',
