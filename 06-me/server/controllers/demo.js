79bb439055afa8851fb57646a22ec271113c2d31
awokelee <awokelee@gmail.com>
2018-09-09 01:04:37 +0800

    refactor: 重构项目目录结构

--- 06-me/server/controllers/demo.js
+++
@@ -1,7 +0,0 @@
-
-
-module.exports = async (ctx)=>{
-  ctx.state.data = {
-    msg:'hello 小程序后台123'
-  }
-}
--- 06-me/src/pages/comments/main.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import Comment from './Comment'
-
-const app = new Vue(Comment)
-app.$mount()
