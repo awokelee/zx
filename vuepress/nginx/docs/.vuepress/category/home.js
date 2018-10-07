b192427128923ffae6a6a91eae05be4406912b23
awokelee <awokelee@gmail.com>
2018-10-08 01:47:17 +0800

    refactor: react 单独拆分

---
+++ vuepress/nginx/docs/.vuepress/category/home.js
@@ -0,0 +1,17 @@
+module.exports = {
+  getList(){
+    return  [
+      '',
+      'win-open-with-vscode',
+      'mac-soft',
+      'vscode',
+      'eslint',
+      'securecrt',
+      'pm2',
+      'puppeteer在linux下报错',
+      'node配置babel使用import',
+      // 'promise',
+      // '浏览器兼容性问题',
+    ]
+  }
+}
---
+++ vuepress/nginx/docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+// const home = require('./category/home')
+
+module.exports = {
+  // base: '/vue-press/',
+  title: 'Awoke',
+  description: 'some notes',
+  search: false,
+  sidebarDepth: 2,
+  searchMaxSuggestions: 10,
+  editLinkText: 'Edit this page on GitHub',
+  lastUpdated: true,
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    nav: getNavList(),
+    sidebar: getSidebar()
+  }
+}
+
+function getNavList() {
+  return [
+    { text: '面试', link: '/vue/' },
+  ]
+}
+
+function getSidebar() {
+  return {
+    '/vue/': [
+      {
+        title: '基础',
+        collapsable: false,
+        children: [
+          '1-1_vue-cli工程中每个文件夹和文件的用处',
+        ]
+      },
+    ],
+    '/': [ '', 'contact', 'about']
+  }
+}
